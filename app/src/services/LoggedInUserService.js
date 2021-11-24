391055889cfc1e225983634a661aee3bf399cb32
Sam Davidge <sam@3sidedcube.com>
2021-11-24 10:41:32 +0000

    Ensure test suite passes

--- app/src/services/LoggedInUserService.js
+++ app/src/services/LoggedInUserService.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
+// eslint-disable-next-line camelcase
 const response_error = require('../responses/response.error');
-const logger = require('logger');
+require('logger');
 
 class LoggedInUserService {
 
@@ -11,7 +12,7 @@ class LoggedInUserService {
             if (getLoggedUserError instanceof response_error.ResponseError) {
                 ctx.response.status = getLoggedUserError.statusCode;
                 ctx.response.body = getLoggedUserError.error;
-                return;
+
             } else {
                 ctx.throw(500, `Error loading user info from token - ${getLoggedUserError.toString()}`);
             }
@@ -19,7 +20,8 @@ class LoggedInUserService {
     }
 
     async getLoggedUser(ctx, logger) {
-        var _a;
+        // eslint-disable-next-line no-underscore-dangle
+        let _a;
         logger.debug('[getLoggedUser] Obtaining loggedUser for token');
         if (!ctx.request.header.authorization) {
             logger.debug('[getLoggedUser] No authorization header found, returning');
@@ -27,27 +29,26 @@ class LoggedInUserService {
         }
         try {
             this.token = ctx.request.header.authorization;
-            let baseURL = process.env.CT_URL;
+            const baseURL = process.env.CT_URL;
             const getUserDetailsRequestConfig = {
                 method: 'GET',
                 baseURL,
                 url: `/auth/user/me`,
                 headers: {
-                    'authorization': ctx.request.header.authorization
+                    authorization: ctx.request.header.authorization
                 }
             };
             const response = await axios.default(getUserDetailsRequestConfig);
             logger.debug('[getLoggedUser] Retrieved token data, response status:', response.status);
             if (['GET', 'DELETE'].includes(ctx.request.method.toUpperCase())) {
                 ctx.request.query = { ...ctx.request.query, loggedUser: JSON.stringify(response.data) };
-            }
-            else if (['POST', 'PATCH', 'PUT'].includes(ctx.request.method.toUpperCase())) {
+            } else if (['POST', 'PATCH', 'PUT'].includes(ctx.request.method.toUpperCase())) {
                 ctx.request.body.loggedUser = response.data;
                 ctx.request.body.token = ctx.request.header.authorization;
             }
-        }
-        catch (err) {
+        } catch (err) {
             logger.error('Error getting user data', err);
+            // eslint-disable-next-line no-cond-assign,no-void
             if ((_a = err === null || err === void 0 ? void 0 : err.response) === null || _a === void 0 ? void 0 : _a.data) {
                 throw new response_error.ResponseError(err.response.status, err.response.data, err.response);
             }
