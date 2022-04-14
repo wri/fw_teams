c5d76bea24a319e9cf11d257f9d6b6f27af65a34
Edward Martin <edwardmartin@edwards-mbp.home>
2022-04-14 17:20:55 +0100

    /healthcheck returns 500 when database not connected

--- app/src/routes/v1/fw_teams/healthcheck.router.js
+++ app/src/routes/v1/fw_teams/healthcheck.router.js
@@ -1,10 +1,18 @@
 const Router = require("koa-router");
 const koaSimpleHealthCheck = require("koa-simple-healthcheck");
+const mongoose = require("mongoose");
 
 const router = new Router({
   prefix: "/healthcheck"
 });
 
-router.get("/", koaSimpleHealthCheck());
+router.get(
+  "/",
+  koaSimpleHealthCheck({
+    test: function () {
+      if (mongoose.connection.readyState !== 1) throw new Error("Not connected to database");
+    }
+  })
+);
 
 module.exports = router;
