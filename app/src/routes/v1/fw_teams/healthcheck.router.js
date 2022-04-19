927842ba81dce7943bb4f166b290a98e419285f6
Edward Martin <edwardmartin@Edwards-MacBook-Pro.local>
2022-04-19 09:32:59 +0100

    Add logging to database connection healthcheck

--- app/src/routes/v1/fw_teams/healthcheck.router.js
+++ app/src/routes/v1/fw_teams/healthcheck.router.js
@@ -1,6 +1,7 @@
 const Router = require("koa-router");
 const koaSimpleHealthCheck = require("koa-simple-healthcheck");
 const mongoose = require("mongoose");
+const logger = require("logger");
 
 const router = new Router({
   prefix: "/healthcheck"
@@ -10,7 +11,11 @@ router.get(
   "/",
   koaSimpleHealthCheck({
     test: function () {
-      if (mongoose.connection.readyState !== 1) throw new Error("Not connected to database");
+      if (mongoose.connection.readyState !== 1) {
+        const error = new Error("Not connected to database");
+        logger.error(error);
+        return { error: error.message };
+      }
     }
   })
 );
