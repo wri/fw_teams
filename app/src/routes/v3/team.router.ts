6f53c4cef0f125c7b13f74d45e9628475f1831d1
owenjs <owen.evans115@gmail.com>
2022-04-20 17:54:04 +0100

    WIP - return all teams by userID

--- app/src/routes/v3/team.router.ts
+++ app/src/routes/v3/team.router.ts
@@ -1,18 +1,29 @@
 import Router from "koa-router";
 import { TeamModel } from "models";
+import { authMiddleware } from "middlewares";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import TeamSerializer from "serializers/team.serializer";
 
-const router = new Router({
-  prefix: "/teams"
-});
+const router = new Router();
 
 // GET v3/teams
-router.get("/", async ctx => {
-  const teams = await TeamModel.find();
+router.get("/myteams", authMiddleware, async ctx => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const loggedUser = JSON.parse(ctx.request.query.loggedUser);
 
-  ctx.body = TeamSerializer.serialize(teams);
+  console.log(loggedUser.id);
+
+  const teams = await TeamModel.find({
+    $or: [
+      { "managers.id": loggedUser.id },
+      { "confirmedUsers.id": loggedUser.id },
+      { "sentInvitations.id": loggedUser.id }
+    ]
+  });
+
+  ctx.body = teams;
 });
 
 export default router;
