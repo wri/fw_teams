538f0dde4da808b6c4b3b66b3eb1eef62da62410
owenjs <owen.evans115@gmail.com>
2022-04-26 14:27:56 +0100

    /v3/teams/leave/:teamId tests added

--- app/src/routes/v3/leaveTeam.router.ts
+++ app/src/routes/v3/leaveTeam.router.ts
@@ -1,5 +1,6 @@
 import Router from "koa-router";
 import { TeamModel } from "models/team.model";
+import { authMiddleware } from "middlewares";
 import teamSerializer from "serializers/teamWithUserRole.serializer";
 
 const router = new Router();
@@ -12,8 +13,8 @@ type TParams = {
   teamId: string;
 };
 
-// GET /v3/teams/leave
-router.get("/teams/leave/:teamId", async ctx => {
+// GET /v3/teams/leave/:teamId
+router.get("/teams/leave/:teamId", authMiddleware, async ctx => {
   const { teamId } = <TParams>ctx.params;
   const query = <TQuery>ctx.request.query;
   const { id: userId } = JSON.parse(query.loggedUser); // ToDo: loggedUser Type
@@ -21,12 +22,18 @@ router.get("/teams/leave/:teamId", async ctx => {
   // Find the team in DB
   const team = await TeamModel.findById(teamId);
 
-  // Remove user from confirmedUser (Monitor)
-  team.confirmedUsers = team.confirmedUsers.filter(confirmedUser => confirmedUser.id !== userId);
-
-  // Remove user from manager area, if not the only manager
-  if (team.managers.some(manager => manager.id !== userId)) {
+  if (team.confirmedUsers.some(confirmedUser => confirmedUser.id === userId)) {
+    // Remove user from confirmedUsers (Monitor)
+    team.confirmedUsers = team.confirmedUsers.filter(confirmedUser => confirmedUser.id !== userId);
+  } else if (team.managers.some(manager => manager.id !== userId) && team.managers.length > 1) {
+    // Remove user from managers array, if not the only manager
     team.managers = team.managers.filter(manager => manager.id !== userId);
+  } else if (team.managers.some(manager => manager.id === userId) && team.managers.length === 1) {
+    ctx.status = 400;
+    throw new Error("User is the only manager of the team and can't leave");
+  } else {
+    ctx.status = 400;
+    throw new Error("User not member of team");
   }
 
   await team.save();
@@ -36,7 +43,7 @@ router.get("/teams/leave/:teamId", async ctx => {
 });
 
 // GET /v3/reject/:teamId
-router.get("/teams/reject/:teamId", async ctx => {
+router.get("/teams/reject/:teamId", authMiddleware, async ctx => {
   const { teamId } = <TParams>ctx.params;
   const query = <TQuery>ctx.request.query;
   const { email: userEmail } = JSON.parse(query.loggedUser); // ToDo: loggedUser Type
--- app/src/routes/v3/leaveTeam.test.ts
+++ app/src/routes/v3/leaveTeam.test.ts
@@ -1,11 +1,36 @@
 import request from "supertest";
-import { TeamModel, ITeam } from "models/team.model";
+import { TeamModel, ITeam, ITeamModel } from "models/team.model";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import loggedInUserService from "services/LoggedInUserService";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import server from "app";
 
+type TTeamDocument = Omit<ITeam, "createdAt">;
+
+const standardTeamDocument: TTeamDocument = {
+  name: "test",
+  confirmedUsers: [
+    {
+      id: "1234TestAuthUser",
+      email: "testAuthUser@test.com"
+    }
+  ],
+  layers: [],
+  areas: [],
+  sentInvitations: [],
+  users: [],
+  managers: [
+    {
+      id: "1234",
+      email: "user@test.com"
+    }
+  ]
+};
+
 describe("GET /v3/teams/leave/:teamId", () => {
-  let teamDocument: Omit<ITeam, "createdAt">;
+  let teamDocument: TTeamDocument, team: ITeamModel;
 
   afterAll(async () => {
     await TeamModel.remove({});
@@ -13,36 +38,115 @@ describe("GET /v3/teams/leave/:teamId", () => {
   });
 
   beforeEach(() => {
-    teamDocument = {
-      name: "test",
-      confirmedUsers: [
-        {
-          id: "1234TestAuthUser",
-          email: "testAuthUser@test.com"
-        }
-      ],
-      layers: [],
-      areas: [],
-      sentInvitations: [],
-      users: [],
-      managers: [
-        {
-          id: "1234",
-          email: "user@test.com"
-        }
-      ]
-    };
+    teamDocument = standardTeamDocument;
   });
 
-  const exec = async () => {
-    const team = await new TeamModel(teamDocument).save();
+  const exec = async (teamId?: string) => {
+    team = await new TeamModel(teamDocument).save();
 
-    return request(server).get(`/v3/teams/leave/${team.id}`);
+    return request(server).get(`/v3/teams/leave/${teamId || team.id}`);
   };
 
   it("should return 200 status", async () => {
     const res = await exec();
 
     expect(res.status).toBe(200);
+  });
+
+  // should return 403 when user is not authorised
+
+  it("should return the updated team", async () => {
+    const res = await exec();
+
+    expect(res.body.data.id).toBe(team.id);
+    expect(res.body.data.type).toBe("team");
+  });
+
+  it("should remove 1234TestAuthUser from confirmed users array in the DB", async () => {
+    await exec();
+
+    const updatedTeam = await TeamModel.findById(team._id);
+
+    expect(updatedTeam.confirmedUsers.length).toBe(0);
+  });
+
+  it("should return 404 when team isn't found", async () => {
+    const res = await exec("1234NotFound");
+
+    expect(res.status).toBe(404);
+  });
+
+  it("should return 400, when authorised user is the only manager of the team", async () => {
+    teamDocument = {
+      ...standardTeamDocument,
+      managers: [
+        {
+          id: "1234TestAuthUser",
+          email: "testAuthUser@test.com"
+        }
+      ],
+      confirmedUsers: [
+        {
+          id: "1234",
+          email: "user@test.com"
+        }
+      ]
+    };
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+    expect(res.body.errors[0].detail).toContain("only manager");
+  });
+
+  it("should return 400, when the authorised user isn't a member of the team", async () => {
+    teamDocument = {
+      ...standardTeamDocument,
+      managers: [
+        {
+          id: "1234",
+          email: "user@test.com"
+        }
+      ],
+      confirmedUsers: [
+        {
+          id: "1234",
+          email: "user@test.com"
+        }
+      ]
+    };
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should remove 1234TestAuthUser from managers array in the DB, when authorised user is not the only manager of the team", async () => {
+    teamDocument = {
+      ...standardTeamDocument,
+      managers: [
+        {
+          id: "1234TestAuthUser",
+          email: "testAuthUser@test.com"
+        },
+        {
+          id: "1234",
+          email: "user@test.com"
+        }
+      ],
+      confirmedUsers: [
+        {
+          id: "1234",
+          email: "user@test.com"
+        }
+      ]
+    };
+
+    await exec();
+
+    const updatedTeam = await TeamModel.findById(team._id);
+
+    expect(updatedTeam.managers.length).toBe(1);
+    expect(updatedTeam.managers[0].id).not.toBe("1234TestAuthUser");
   });
 });
