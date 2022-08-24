a87d94fed1484ddab67b9c101fa53a96fb3ffec8
edward3sc <edward3sc@users.noreply.github.com>
2022-08-24 11:02:26 +0000

    auto commit: apply eslint fixes

--- app/src/routes/v3/teams/teamUsers.router.ts
+++ app/src/routes/v3/teams/teamUsers.router.ts
@@ -12,7 +12,7 @@ import {
 } from "models/teamUserRelation.model";
 import serializeTeamUser from "serializers/teamUserRelation.serializer";
 import TeamUserRelationService from "services/teamUserRelation.service";
-const logger = require("logger")
+const logger = require("logger");
 
 const router = new Router({
   prefix: "/:teamId/users"
@@ -163,8 +163,12 @@ router.delete(
 
     if (!teamUser) ctx.throw(404, "This team user relation doesn't exist");
 
-    let authorised = teamUser && (teamUser.role === EUserRole.Administrator || teamUser.role === EUserRole.Manager || teamUser.userId?.toString() === loggedUserId)
-    logger.info(authorised, teamUser)
+    const authorised =
+      teamUser &&
+      (teamUser.role === EUserRole.Administrator ||
+        teamUser.role === EUserRole.Manager ||
+        teamUser.userId?.toString() === loggedUserId);
+    logger.info(authorised, teamUser);
     if (!authorised) ctx.throw(400, "You are not authorized to remove this user from this team");
 
     if (teamUser.role === EUserRole.Administrator) {
