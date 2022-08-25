da3d84c58a923afa531b982ab0e33f318faa34b1
Edward Martin <edward@3sidedcube.com>
2022-08-25 15:05:24 +0100

    return null for null userId

--- app/src/services/user.service.js
+++ app/src/services/user.service.js
@@ -28,7 +28,6 @@ class UserService {
 
   static async getNameByIdMICROSERVICE(userId) {
     if(!userId) return null
-    logger.info("Get user by user id", userId);
     try {
       let baseURL = config.get("usersApi.url");
       const response = await axios.default({
@@ -41,12 +40,11 @@ class UserService {
       });
       const user = response.data;
       if (!user || !user.data) return null;
-      logger.info("Got user by user id", user);
       return user.data.attributes.firstName
         ? `${user.data.attributes.firstName} ${user.data.attributes.lastName}`
         : user.data.attributes.lastName;
     } catch (e) {
-      logger.info("Error finding user", e);
+      logger.info(`Error finding user ${userId}`, e);
       return null;
     }
   }
